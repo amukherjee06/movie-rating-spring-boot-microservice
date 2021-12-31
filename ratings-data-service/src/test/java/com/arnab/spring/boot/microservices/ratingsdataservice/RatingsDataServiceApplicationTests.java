d1241fa395b9417dbac66ea18b11fbd7e3d213a4
Arnab Mukherjee <mukherjeearnab06@gmail.com>
2021-12-31 17:52:45 +0530

    Inital Commit for Movie Rating Service

---
+++ ratings-data-service/src/test/java/com/arnab/spring/boot/microservices/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.boot.microservices.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
